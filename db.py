bf717c1d2ba05fde6f26683c4ac71217a40962f2
Юрий <urtets@gmail.com>
2024-12-28 15:30:50 +0300

    Module_14 tasks

--- db.py
+++ db.py
@@ -14,5 +14,13 @@ age INTEGER
 ''')
 
 cursor.execute(" CREATE INDEX IF NOT EXISTS idx_email ON Users (email)")
+
+# cursor.execute(" INSERT INTO Users (username, email, age) VALUES (?, ?, ?)", ("newuser", "ex@gmail.com","28"))
+# for i in range(30):
+#     cursor.execute(" INSERT INTO Users (username, email, age) VALUES (?, ?, ?)", (f"newuser{i}", f"{i}ex@gmail.com", "28"))
+
+# cursor.execute("UPDATE Users SET age = ? WHERE username = ?", (29, "newuser"))
+
+cursor.execute("DELETE FROM Users WHERE username = ?", ("newuser",))
 connection.commit()
 connection.close()
